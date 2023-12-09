18fadf17c40fc5c16a7a4a927cd68dc6269d17bb
MalachiMcClure <jptBWe249781@outlook.com>
2023-12-09 15:27:54 +0800

    quibusdam

---
+++ Turn.Server/AllocationsPool.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using Turn.Message;
+using SocketServers;
+
+namespace Turn.Server
+{
+    class AllocationsPool
+    {
+        public enum RemoveReason
+        {
+            Timeout,
+            Replace,
+            Stopping,
+        }
+
+        public delegate void AllocationsPoolDelegate(Allocation allocation, RemoveReason reason);
+
+        private object syncRoot;
+        private Dictionary<ServerEndPoint, Allocation> byAllocated;
+        private Dictionary<ServerEndPoint, Allocation> byReal;
+        private Dictionary<ConnectionId, Allocation> byConnectionId;
+        private Dictionary<string, Allocation> byKey;
+        private Timer timer;
+
+        public AllocationsPool()
+        {
+            syncRoot = new object();
+            byAllocated = new Dictionary<ServerEndPoint, Allocation>();
+            byReal = new Dictionary<ServerEndPoint, Allocation>();
+            byConnectionId = new Dictionary<ConnectionId, Allocation>();
+            byKey = new Dictionary<string, Allocation>();
+            timer = new Timer(Timer_EventHandler, null, 0, 10000);
+        }
+
+        public event AllocationsPoolDelegate Removed;
+
+        public void Replace(Allocation allocation)
+        {
+            Allocation oldAllocation;
+
+            lock (syncRoot)
+            {
+                var key = GetKey(allocation.Local, allocation.Reflexive);
+
+                if (byKey.TryGetValue(key, out oldAllocation))
+                {
+                    byAllocated.Remove(oldAllocation.Alocated);
+                    byReal.Remove(oldAllocation.Real);
+                    byConnectionId.Remove(oldAllocation.ConnectionId);
+                    byKey.Remove(key);
+                }
+
+                byAllocated.Add(allocation.Alocated, allocation);
+                byReal.Add(allocation.Real, allocation);
+                byConnectionId.Add(allocation.ConnectionId, allocation);
+                byKey.Add(key, allocation);
+            }
+
+            if (oldAllocation != null)
+                OnRemoved(oldAllocation, RemoveReason.Replace);
+        }
+
+        public void Remove(Allocation oldAllocation, RemoveReason reason)
+        {
+            lock (syncRoot)
+            {
+                byAllocated.Remove(oldAllocation.Alocated);
+                byReal.Remove(oldAllocation.Real);
+                byConnectionId.Remove(oldAllocation.ConnectionId);
+                byKey.Remove(GetKey(oldAllocation.Local, oldAllocation.Reflexive));
+            }
+
+            OnRemoved(oldAllocation, reason);
+        }
+
+        public void Clear()
+        {
+            Allocation[] removeList = null;
+
+            lock (syncRoot)
+            {
+                if (byAllocated.Values.Count > 0)
+                {
+                    removeList = new Allocation[byAllocated.Values.Count];
+                    byAllocated.Values.CopyTo(removeList, 0);
+                }
+
+                byAllocated.Clear();
+                byReal.Clear();
+                byConnectionId.Clear();
+                byKey.Clear();
+            }
+
+            if (removeList != null)
+                foreach (var allocation in removeList)
+                    OnRemoved(allocation, RemoveReason.Stopping);
+        }
+
+        public Allocation Get(ServerEndPoint allocated)
+        {
+            Allocation allocation = null;
+
+            lock (syncRoot)
+            {
+                if (byAllocated.TryGetValue(allocated, out allocation) == false)
+                    byReal.TryGetValue(allocated, out allocation);
+            }
+
+            return allocation != null && allocation.IsValid() ? allocation : null;
+        }
+
+        public Allocation Get(ConnectionId connectionId)
+        {
+            Allocation allocation = null;
+
+            lock (syncRoot)
+                byConnectionId.TryGetValue(connectionId, out allocation);
+
+            return allocation != null && allocation.IsValid() ? allocation : null;
+        }
+
+        public Allocation GetByPeer(ServerEndPoint local, IPEndPoint remote)
+        {
+            Allocation allocation = null;
+
+            lock (syncRoot)
+                byKey.TryGetValue(GetKey(local, remote), out allocation);
+
+            return allocation != null && allocation.IsValid() ? allocation : null;
+        }
+
+        public int Count
+        {
+            get
+            {
+                lock (syncRoot)
+                    return byAllocated.Count;
+            }
+        }
+
+        private string GetKey(ServerEndPoint local, IPEndPoint remote)
+        {
+            return local.ToString() + @"|" + remote.ToString();
+        }
+
+        private void Timer_EventHandler(Object stateInfo)
+        {
+            List<Allocation> removeList = null;
+
+            lock (syncRoot)
+            {
+                int now = Environment.TickCount;
+
+                foreach (var tuple in byAllocated)
+                    if (tuple.Value.IsValid(now) == false)
+                    {
+                        if (removeList == null)
+                            removeList = new List<Allocation>();
+                        removeList.Add(tuple.Value);
+                    }
+            }
+
+            if (removeList != null)
+                foreach (var allocation in removeList)
+                    Remove(allocation, RemoveReason.Timeout);
+        }
+
+        private void OnRemoved(Allocation allocation, RemoveReason reason)
+        {
+            Removed(allocation, reason);
+        }
+    }
+}
---
+++ Turn.Server/ConnectionIdGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Security.Cryptography;
+using SocketServers;
+using Turn.Message;
+
+namespace Turn.Server
+{
+	static class ConnectionIdGenerator
+	{
+		private static byte[] sha1Key;
+        private static long count;
+		[ThreadStatic]
+		private static HMACSHA1 sha1;
+
+		static ConnectionIdGenerator()
+		{
+			sha1Key = new byte[8];
+			(new Random(Environment.TickCount)).NextBytes(sha1Key);
+		}
+
+		public static ConnectionId Generate(ServerEndPoint reflexive, IPEndPoint local)
+		{
+			if (sha1 == null)
+				sha1 = new HMACSHA1(sha1Key);
+
+            string hashData = reflexive.ToString() + local.ToString() + Interlocked.Increment(ref count).ToString();
+
+			var bytes = sha1.ComputeHash(Encoding.UTF8.GetBytes(hashData));
+
+			return new ConnectionId()
+			{
+				Value1 = BitConverter.ToInt64(bytes, 0),
+				Value2 = BitConverter.ToInt64(bytes, 8),
+				Value3 = BitConverter.ToInt32(bytes, 16),
+			};
+		}
+	}
+}
